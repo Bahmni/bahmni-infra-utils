f78b1ea82f6b6c423eeb8db5043b65e662c899a9
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2026-03-23 11:09:51 +0530

    BAH-4548 | Fix. Trivy to patche version 0.69.3 (#13)

--- trivy_scan.sh
+++ trivy_scan.sh
@@ -5,7 +5,7 @@ sudo apt-get install wget apt-transport-https gnupg lsb-release
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
-sudo apt-get install trivy
+sudo apt-get install trivy=0.69.3
 
 #Make an array of input paths
 TARGETS=("$@")
